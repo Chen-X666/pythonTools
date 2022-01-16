ec69eac00b5d281980d35bafc21f987752e514ec
ChenX <972113786@qq.com>
2022-01-16 15:36:32 +0800

    push

--- csvProcess/mergeAllCsv.py
+++ csvProcess/mergeAllCsv.py
@@ -6,41 +6,28 @@ Version:  V 0.1
 File:     mergeAllCsv.py
 Describe:  Github link: https://github.com/Chen-X666
 """
+import csv
 import os
+import glob
+import os
+import pandas as pd
 
 import pandas as pd
 
 import datetime
 
-def getFileName(path):
-    files = os.listdir(path)
-    # print(files)
-    filenames = []
-    for file in files:
-        filenames.append(file)
-    # print(filenames)
-    return filenames
+inputfile = str(os.path.dirname(os.getcwd())) + "\\csvProcess\\CandidateWordResult\\*.csv"
+print(inputfile)
+outputfile = str(os.path.dirname(os.getcwd())) + "\\csvProcess\\trainingData4.csv"
+csv_list = glob.glob(inputfile)
 
-if __name__ == '__main__':
-    # 合并下面文件下所有excel文件：
-    path = r'D:\小的项目\旅游分析\数据'
-    # 读取该文件夹下的需要合并的文件路径
-    filelist = getFileName(path)
-    # 设置合并后文件的名字
-    file_time = datetime.datetime.now()
-    file_time = str(file_time).replace(' ', '_').replace(':','_').replace('-','_')[:-7]
-    savename = file_time + '_合并文件.xlsx'
-    # 存储合并的df
-    savedata = []
-    # 依次读取待合并数据：
-    for file in filelist:
-        # 读写文件
-        filepath = path + "\\" + file
-        print('需要合并的文件：', filepath)
-        df = pd.read_excel(filepath)
-        savedata.append(df)
-        # 合并文件
-        newdf = pd.concat(savedata)
-        # 保存文件
-        newdf.to_excel('./data/' + savename, index=False) # 保存文件，为追加模式，且不要列头
-        print('合并后数据的大小：', newdf.shape)
+filepath = csv_list[0]
+df = pd.read_csv(filepath,encoding='GBK')
+df = df.to_csv(outputfile, index=False)
+
+for i in range(1, len(csv_list)):
+    print(csv_list[i])
+    filepath = csv_list[i]
+    df = pd.read_csv(filepath,encoding='GBK',error_bad_lines=False)
+    df = df.to_csv(outputfile, index=False, header=False, mode='a+')
+print('finished')
--- csvProcess/mergeAllTxt.py
+++ csvProcess/mergeAllTxt.py
@@ -8,6 +8,7 @@ Describe:  Github link: https://github.com/Chen-X666
 """
 import os
 
+
 def mergeAllTxt(filesPath):
     # coding=utf-8
     # 获取目标文件夹的路径
@@ -15,23 +16,26 @@ def mergeAllTxt(filesPath):
     # 获取当前文件夹中的文件名称列表
     filenames = os.listdir(filedir)
     # 打开当前目录下的result.txt文件，如果没有则创建
-    f = open('result.txt', 'w',encoding='utf-8')
+    f = open('result.txt', 'w', encoding='utf-8')
     # 先遍历文件名
     for filename in filenames:
         print(filename)
         filepath = filedir + '/' + filename
         # 遍历单个文件，读取行数
-        for line in open(filepath,encoding='utf-8'):
+        for line in open(filepath, encoding='GBK',errors='ignore'):
             f.writelines(line)
-        f.write('\n')
+        #f.write('\n')
     # 关闭文件
     f.close()
-    
+
+
 def getTxtByLineToList(filePath):
-    file = open(filePath, encoding='utf-8')
+    file = open(filePath, encoding='GBK')
     line = file.read().splitlines()
     file.close()
     return line
 
+
 if __name__ == '__main__':
-    mergeAllTxt('C:/Users/Chen/Desktop/pythonTools/txtprocess/files')
+    mergeAllTxt('C:/Users/Chen/Desktop/pythonTools/csvProcess/mergeCsvData')
+    print('over')
